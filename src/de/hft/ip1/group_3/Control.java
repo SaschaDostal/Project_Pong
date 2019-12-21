e81aa0beb160410081bf4340d467cce247f16d93
Felix Koerner <91kofe1bif@hft-stuttgart.de>
2019-12-21 10:07:59 +0100

    initial commit

---
+++ src/de/hft/ip1/group_3/Control.java
@@ -0,0 +1,10 @@
+package de.hft.ip1.group_3;
+
+public class Control {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/de/hft/ip1/group_3/GameComponent.java
@@ -0,0 +1,7 @@
+package de.hft.ip1.group_3;
+
+import javax.swing.JComponent;
+
+public abstract class GameComponent extends JComponent {
+
+}
