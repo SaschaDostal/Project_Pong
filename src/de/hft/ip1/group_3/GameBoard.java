b0faf67b843b4b80d492f87ba533a2e7f1117e25
Felix Körner <91kofe1bif@hft-stuttgart>
2019-12-22 11:53:38 +0100

    implement GameBoard class

--- src/de/hft/ip1/group_3/GameBoard.java
+++ src/de/hft/ip1/group_3/GameBoard.java
@@ -1,42 +1,77 @@
 package de.hft.ip1.group_3;
 
 import java.awt.BorderLayout;
-import java.awt.EventQueue;
+import java.util.NoSuchElementException;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.Color;
 
 public class GameBoard extends JFrame {
 
-	private JPanel contentPane;
+    private static final long serialVersionUID = 1L;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					GameBoard frame = new GameBoard();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+    private JPanel contentPane;
+    private GameComponent[] gameComponents;
+    private Position[] validPositions;
 
-	/**
-	 * Create the frame.
-	 */
-	public GameBoard() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
-		setContentPane(contentPane);
-	}
+    /**
+     * Create the frame.
+     */
+    public GameBoard(GameComponent[] gameComponents) {
+        this.gameComponents = gameComponents;
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100, 100, 450, 300);
+        contentPane = new JPanel();
+        contentPane.setBackground(Color.BLACK);
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        contentPane.setLayout(new BorderLayout(0, 0));
+        
+        for (GameComponent elem : gameComponents) {
+            contentPane.add(elem);
+        }
+        contentPane.revalidate();
+        contentPane.repaint();
+        
+        setContentPane(contentPane);
+    }
+
+    public Ball getBall() throws NoSuchElementException {
+        for (GameComponent elem : gameComponents) {
+            if (elem.getClass().equals(Ball.class)) {
+                return (Ball) elem;
+            }
+        }
+        throw new NoSuchElementException("Whoops, looks like we lost the ball");
+    }
+
+    public PlayerBar getPlayerBar1() throws NoSuchElementException {
+        for (GameComponent elem : gameComponents) {
+            if (elem.getClass().equals(PlayerBar.class) && ((PlayerBar) elem).getId() == 0) {
+                return (PlayerBar) elem;
+            }
+        }
+        throw new NoSuchElementException("Whoops, looks like we lost the bar");
+    }
+
+    public PlayerBar getPlayerBar2() throws NoSuchElementException {
+        for (GameComponent elem : gameComponents) {
+            if (elem.getClass().equals(PlayerBar.class) && ((PlayerBar) elem).getId() == 1) {
+                return (PlayerBar) elem;
+            }
+        }
+        throw new NoSuchElementException("Whoops, looks like we lost the bar");
+    }
+
+    public GameComponent[] getGameComponents() {
+        return gameComponents;
+    }
+
+    public void draw() {
+        //TODO: for some reason only the first PlayerBar is being repainted
+        this.repaint();
+    }
 
 }
