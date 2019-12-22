9acf96fcab740d6bde6b3b6d5da17822a59aaecd
Felix Körner <91kofe1bif@hft-stuttgart>
2019-12-22 11:52:28 +0100

    implement Ball class

--- src/de/hft/ip1/group_3/Ball.java
+++ src/de/hft/ip1/group_3/Ball.java
@@ -1,5 +1,28 @@
 package de.hft.ip1.group_3;
 
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Rectangle;
+
 public class Ball extends GameComponent {
+
+    private static final long serialVersionUID = 1L;
+
+    private int speed;
+
+    public Ball(int id, Position pos, boolean visible, Rectangle hitbox, int speed) {
+        super(id, pos, visible, hitbox);
+        this.speed = speed;
+    }
+
+    public int getSpeed() {
+        return this.speed;
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        g.setColor(Color.WHITE);
+        g.fillOval(pos.getX(), pos.getY(), 4, 4);
+    }
 
 }
