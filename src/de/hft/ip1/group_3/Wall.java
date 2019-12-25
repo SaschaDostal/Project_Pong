44d35a9bd31cd491961c9ee8306ed431ee59c94d
Felix Körner <91kofe1bif@hft-stuttgart>
2019-12-25 12:14:45 +0100

    add new classes implement constructor and abstract method in Wall class

--- src/de/hft/ip1/group_3/Wall.java
+++ src/de/hft/ip1/group_3/Wall.java
@@ -1,5 +1,19 @@
 package de.hft.ip1.group_3;
 
+import java.awt.Graphics;
+import java.awt.Rectangle;
+
 public class Wall extends GameComponent{
+
+    public Wall(int id, Position pos, boolean visible, Rectangle hitbox) {
+        super(id, pos, visible, hitbox);
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        // TODO Auto-generated method stub
+        
+    }
 
 }
