f8aac0f7c5558ea1b1333743aeda95334973a791
Sascha Dostal <91dosa1bif@hft-stuttgart.de>
2019-12-27 12:04:26 +0100

    implement ScoringSystem

--- src/de/hft/ip1/group_3/ScoringSystem.java
+++ src/de/hft/ip1/group_3/ScoringSystem.java
@@ -1,5 +1,50 @@
 package de.hft.ip1.group_3;
 
-public class ScoringSystem {
+import java.security.InvalidParameterException;
 
+public class ScoringSystem {
+	private int pointsPlayer1 = 0;
+	private int pointsPlayer2 = 0;
+	private int pointsPlayer3 = 0;
+	private int pointsPlayer4 = 0;
+	private int numberOfPlayers;
+	
+	ScoringSystem (int numberOfPlayers){
+		this.numberOfPlayers = numberOfPlayers;
+	}
+	
+	public int getPointsOfPlayer(int playerNumber) throws InvalidParameterException {
+		if (playerNumber > numberOfPlayers) {
+			throw new InvalidParameterException("Player does not exist!");
+		}
+		int playerPoints;
+		switch(playerNumber) {
+		case 1: playerPoints = pointsPlayer1; break;
+		case 2: playerPoints = pointsPlayer2; break;
+		case 3: playerPoints = pointsPlayer3; break;
+		case 4: playerPoints = pointsPlayer4; break;
+		default: throw new InvalidParameterException("Player does not exist!");
+		}
+		return playerPoints;
+	}
+	
+	public void addPointToPlayer(int playerNumber) {
+		if (playerNumber > numberOfPlayers) {
+			throw new InvalidParameterException("Player does not exist!");
+		}
+		switch(playerNumber) {
+		case 1: pointsPlayer1++; break;
+		case 2: pointsPlayer2++; break;
+		case 3: pointsPlayer3++; break;
+		case 4: pointsPlayer4++; break;
+		default: throw new InvalidParameterException("Player does not exist!");
+		}
+	}
+	
+	public void resetPlayerPoints() {
+		pointsPlayer1 = 0;
+		pointsPlayer2 = 0;
+		pointsPlayer3 = 0;
+		pointsPlayer4 = 0;
+	}
 }
