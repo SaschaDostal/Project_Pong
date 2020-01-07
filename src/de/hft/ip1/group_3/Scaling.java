ee9234fcc476a8cfa797413e26a9d7205ac3ab73
Sascha Dostal <91dosa1bif@hft-stuttgart.de>
2020-01-07 15:16:01 +0100

    implement fields for scaling

--- src/de/hft/ip1/group_3/Scaling.java
+++ src/de/hft/ip1/group_3/Scaling.java
@@ -1,5 +1,31 @@
 package de.hft.ip1.group_3;
 
 public class Scaling {
-
+    public static int sizeX;
+    public static int sizeY;
+    
+    public static int playerBarRec1X;
+    public static int playerBarRec1Y;
+    public static int playerBarPos1X;
+    public static int playerBarPos1Y;
+    
+    public static int playerBarRec2X;
+    public static int playerBarRec2Y;
+    public static int playerBarPos2X;
+    public static int playerBarPos2Y;
+    
+    public static int wallRec1X;
+    public static int wallRec1Y;
+    public static int wallPos1X;
+    public static int wallPos1Y;
+    
+    public static int wallRec2X;
+    public static int wallRec2Y;
+    public static int wallPos2X;
+    public static int wallPos2Y;
+    
+    public static int ballRecX;
+    public static int ballRecY;
+    public static int ballPosX;
+    public static int ballPosY;
 }
